a941ff8105b71b44dd4f5b755cd9e376ff217c13
Gaelle Gilles <67564107+gia534@users.noreply.github.com>
2021-05-31 09:43:53 -0400

    Layout of book club application

---
+++ WhereverYouAreBookClubs/app/src/main/java/com/gia/whereveryouarebookclubs/NonFictionGenre.java
@@ -0,0 +1,14 @@
+package com.gia.whereveryouarebookclubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NonFictionGenre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_fiction_genre);
+    }
+}
---
+++ WhereverYouAreBookClubs/app/src/main/java/com/gia/whereveryouarebookclubs/newBookClub.java
@@ -0,0 +1,14 @@
+package com.gia.whereveryouarebookclubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newBookClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_book_club);
+    }
+}
